67ca9bf289507158cc1ef8b0cfac8588f2fb6103
Jesse Guillory <jhowder91@gmail.com>
2018-09-19 18:35:47 -0500

    Added Enemy class

---
+++ ab24f15/ConsoleApp1/ConsoleApp1/Enemy.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game1
+{
+    class Enemy
+    {
+
+        string name;
+        public double health;
+        public double mana;
+
+        public double strength;
+        public double intelligence;
+        public double dexterity;
+        public double vitality;
+        public double wisdom;
+
+        public Enemy(int hp, int mana, int str, int intel, int dex, int vit, int wis)
+        {
+            health = hp;
+            this.mana = mana;
+            strength = str;
+            intelligence = intel;
+            dexterity = dex;
+            vitality = vit;
+            wisdom = wis;
+        }
+    }
+}
